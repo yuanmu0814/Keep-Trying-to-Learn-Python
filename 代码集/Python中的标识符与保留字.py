97e448b4dd96ec961ca4d68e8bbb469faefca7ff
Mu Yuan <yuanmu.email@gmail.com>
2023-07-12 19:45:46 +0800

    Add files via upload

---
+++ 代码集/Python中的标识符与保留字.py
@@ -0,0 +1,13 @@
+# 使用Python语言编写
+# 开发时间 2023/7/12 19:17
+
+# Python中的标识符和保留字
+# 保留字：
+# 有一些单词被Python赋予了特定的意义，这些单词在给使用者给任何对象起名字时都不能用
+# 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'
+# 标识符：
+# 对变量、函数、类、模块和其他对象起的名字叫做标识符
+# 可以使用：字母、数字、下划线
+# 不能以数字开头
+# 不能使用保留字
+# 严格区分大小写
---
+++ 代码集/keyword_demo.py
@@ -0,0 +1,5 @@
+# 使用Python语言编写
+# 开发时间 2023/7/12 19:44
+
+import keyword
+print(keyword.kwlist)
