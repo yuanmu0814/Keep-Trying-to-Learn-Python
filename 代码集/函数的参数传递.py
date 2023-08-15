428f955fae3f4cd1deb8455f75289069ffa224f1
yuanmu0814 <yuanmu.email@gmail.com>
2023-08-15 10:18:16 +0000

    release

--- 代码集/函数的参数传递.py
+++ 代码集/函数的参数传递.py
@@ -10,5 +10,5 @@ def calc(a, b):  # a，b称为形式参数，简称形参，形参的位置是�
 result = calc(10, 20)  # 10，20称为实际参数的值，简称实参，实参的位置是函数的调用处
 print(result)
 
-result = calc(b = 10, a = 20)
+result = calc(b = 10, a = 20)  # =左侧的变量的名称称为关键字参数
 print(result)
